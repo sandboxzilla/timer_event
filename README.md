70065d71d4c7108cf7af49b9f2aa3755c1de31fb
Erol Yesin <5965645+erolyesin@users.noreply.github.com>
2023-03-12 22:35:14 -0700

    Final touches to the setup.py

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Timer Event Package
 
-The Timer Event package provides classes for creating timed events and subscribing to them. The package includes the following classes:
+The Timer Event package provides classes for creating timed threaded events and subscribing to them. The package includes the following classes:
 *TimerEvent:* A class for creating timed events that trigger at a specified interval.
 *Event:* A class for creating events that can be subscribed to and triggered with a payload.
 
--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
 
 
 setup(
     name='TimerEvent',
+    packages=find_packages('src'),
     version='0.7',
     license='MIT',
     author="Erol Yesin",
     author_email='erol@sandboxzilla.com',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    url='https://github.com/erolyesin/timer_event',
-    keywords='python timer event',
+    description='The Timer Event package provides classes for creating timed threaded events',
+    download_url='https://github.com/user/erolyesin/release/v_07.tar.gz',
+    keywords=['Event', 'Thread', 'Timer'],
     install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License', 
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
 )
